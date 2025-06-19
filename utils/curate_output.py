99b6eee8b8a577e5848a123ba29da4380612ba79
hajerkr <heajer.karoui@gmail.com>
2025-06-19 17:22:26 +0100

    clean file

--- utils/curate_output.py
+++ utils/curate_output.py
@@ -199,25 +199,3 @@ def demo(context):
     log.info(f"Demographics:  {subject_label} {session_label} {str(age_in_months)} {sex}")
 
     return demo
-
-    # -------------------  Concatenate the data  -------------------  #
-
-def housekeeping(demographics):
-
-    acq = demographics['acquisition'].values[0]
-    sub = demographics['subject'].values[0]
-
-    filePath = '/flywheel/v0/work/All_volumes.csv'
-    volumes = pd.read_csv(filePath, sep='\s+', engine='python') #index_col=False,
-    df = pd.concat([demographics.reset_index(drop=True), volumes.reset_index(drop=True)], axis=1)
-    out_name = f"{acq}_volumes.csv"
-    outdir = ('/flywheel/v0/output/' + out_name)
-    df.to_csv(outdir, index=False)
-
-    seg_file = '/flywheel/v0/work/Final_segmentation_atlas_with_callosum.nii.gz'
-    new_seg_file = '/flywheel/v0/output/' + acq + '_segmentation.nii.gz'
-    shutil.copy(seg_file, new_seg_file)
-
-    QC_montage = '/flywheel/v0/work/montage_final_segmentation_atlas_with_callosum.png'
-    new_QC_montage = '/flywheel/v0/output/' + acq + '_QC-montage.png'
-    shutil.copy(QC_montage, new_QC_montage)
